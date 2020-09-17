9ea12f81a0083495876f6f2e2be5181de146a22a
gaurav <gauravravat688@gmail.com>
2020-09-17 14:06:19 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#togbtn").click(function() {
+        $(".container-fluid").toggle();
+    });
+
+    $("#togbtn").click(function() {
+        $('.container-fluid').css("visibility", 'visible');
+    });
+
+});
